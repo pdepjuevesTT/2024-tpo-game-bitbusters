2301935d6b76c89f3bb8ac29256d6d0e79e33b05
Gero <geroxed@gmail.com>
2024-10-23 01:04:10 -0300

    Organize project for release

--- runGame.bat
+++ runGame.bat
@@ -2,10 +2,10 @@
 
 set "scriptDir=%~dp0"
 
-set "wollokDirectory= .\wollok.exe"
+set "wollokDirectory= .\files\wollok.exe"
 
 start http://localhost:3000
 
-%wollokDirectory% run -g "main.TetrisWollok" --skipValidations -p %scriptDir%
+%wollokDirectory% run -g "main.TetrisWollok" --skipValidations -p %scriptDir%files\
 
 pause
